bc5e4690e1bdd37205c3b86f0f2c7bc8b22f1816
Anup Adhikari <anupadhikari269@gmail.com>
2024-01-17 22:21:12 +0545

    added user model,app.js

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new Schema(
+    {
+
+        username:{
+            type:string,
+            required:true,
+            unique:true,
+            trim:true,
+            index:true
+            
+        },
+
+        email:{
+            type:string,
+            required:true,
+            unique:true,
+            
+            
+        },
+        phone:{
+            type:string,
+            unique:true,
+            trim:true
+        },
+        fullName:{
+            type:string,
+            required:true
+
+
+        },
+        avatar:{
+            type:string,
+
+
+        },
+        password:{
+            type:string,
+            required:[true,"Password is required"]
+        },
+        location:{
+            type:string,
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const User= mongoose.model("User",userSchema)
